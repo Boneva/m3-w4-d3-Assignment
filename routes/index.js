4d428705ae15853c3f3e2c2b9962af20e20f9d5d
Boneva <lixinyue1226@gmail.com>
2025-06-20 18:27:24 -0700

    assignment finished

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const auth = require('http-auth');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 const Registration = mongoose.model('Registration');
@@ -41,24 +42,40 @@ router.post('/register',
         check('email')
         .isLength({ min: 1 })
         .withMessage('Please enter an email'),
+        check('username')
+        .isLength({ min: 1 })
+        .withMessage('Please enter a username'),
+        check('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
     ],
-    (req, res) => {
-        //console.log(req.body);
-        const errors = validationResult(req);
-        if (errors.isEmpty()) {
-          const registration = new Registration(req.body);
-          registration.save()
-            .then(() => {res.send('Thank you for your registration!');})
-            .catch((err) => {
-              console.log(err);
-              res.send('Sorry! Something went wrong.');
-            });
-          } else {
-            res.render('register', { 
-                title: 'Registration form',
-                errors: errors.array(),
-                data: req.body,
-             });
+    async (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      try {
+        const registration = new Registration(req.body);
+
+        // generate salt to hash password
+        const salt = await bcrypt.genSalt(10);
+
+        // set user password to hashed password
+        registration.password = await bcrypt.hash(registration.password, salt);
+
+        // save registration
+        await registration.save();
+
+        res.redirect('/thankyou');
+      } catch (err) {
+        console.log(err);
+        res.send('Sorry! Something went wrong.');
+      }
+    } else {
+      res.render('register', {
+        title: 'Registration form',
+        errors: errors.array(),
+        data: req.body,
+      });
           }
     });
 
